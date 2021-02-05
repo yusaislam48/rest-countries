6034d0778101707867272c17b5d36c8c759828a1
Yusa Islam <yusaislam48@gmail.com>
2021-02-05 23:44:27 +0600

    added external css

--- app.js
+++ app.js
@@ -6,15 +6,22 @@ fetch('https://restcountries.eu/rest/v2/all')
         for (let i = 0; i < data.length; i++) {
             const country = data[i];
             const countriesDiv = document.getElementById('countries');
-
             const countryDiv = document.createElement('div');
-            const name = document.createElement('h3');
-            name.innerText = country.name;
-            const capital = document.createElement('p');
-            capital.innerText = country.capital;
-            countryDiv.appendChild(name);
-            countryDiv.appendChild(capital);
 
+            // const name = document.createElement('h3');
+            // name.innerText = country.name;
+            // const capital = document.createElement('p');
+            // capital.innerText = country.capital;
+            // countryDiv.appendChild(name);
+            // countryDiv.appendChild(capital);
+
+            countryDiv.className = 'country';
+
+            const countriesInfo = `
+                <h2><span class="country-name">Country Name: </span>${country.name}</h2>
+                <h4>Capital: <span class="capital-name">${country.capital}</span></h4>    
+            `
+            countryDiv.innerHTML = countriesInfo;
             countriesDiv.appendChild(countryDiv);
         }
     }
